9a0380f20588a76d852b96d7bab460c6621c69d6
lonelystar <491688758@qq.com>
2018-11-24 19:10:06 +0800

    config4

---
+++ multistage-distribution-system-web/src/main/resources/static/static/json/menu.js
@@ -0,0 +1,102 @@
+{
+	"code": 0,
+	"msg": "ok",
+	"data": [{
+		"title": "首页",
+		"icon": "layui-icon-home",
+		"href": "/"
+	}, {
+		"title": "经销商管理",
+		"icon": "layui-icon-unorderedlist",
+		"childs": [{
+			"title": "注册一级经销商",
+			"href": "/list/table/id=1"
+		}, {
+			"title": "经销商信息管理",
+			"href": "/list/card"
+		}]
+	}, {
+		"title": "订单管理",
+		"icon": "layui-icon-container",
+		"childs": [{
+			"title": "订单查看",
+			"href": "/detail/plan"
+		}, {
+			"title": "生成出货单",
+			"href": "/chart/index"
+		}]
+	}, {
+		"title": "发货管理",
+		"icon": "layui-icon-file-exception",
+		"childs": [{
+			"title": "查看出货单列表",
+			"href": "/form/basic"
+		}, {
+			"title": "PDA扫码出货",
+			"href": "/form/group"
+		}]
+	}, {
+		"title": "产品管理",
+		"icon": "layui-icon-error",
+		"childs": [{
+			"title": "403",
+			"href": "/exception/403"
+		}, {
+			"title": "404",
+			"href": "/exception/404"
+		}, {
+			"title": "500",
+			"href": "/exception/500"
+		}]
+	}, {
+		"title": "新增模块",
+		"icon": "layui-icon-experiment",
+		"notice": 3,
+		"childs": [{
+			"title": "admin",
+			"href": "/module/admin"
+		}, {
+			"title": "helper",
+			"href": "/module/helper"
+		}, {
+			"title": "loadBar",
+			"href": "/module/loadbar"
+		}]
+	}, {
+		"title": "图标",
+		"icon": "layui-icon-star",
+		"href": "/icon/index"
+	}, {
+		"title": "多级导航",
+		"icon": "layui-icon-apartment",
+		"childs": [{
+			"title": "Dota2",
+			"childs": [{
+				"title": "敌法师",
+				"childs": [{
+					"title": "法力损毁"
+				}, {
+					"title": "闪烁"
+				}, {
+					"title": "法术护盾"
+				}, {
+					"title": "法力虚空"
+				}]
+			}, {
+				"title": "帕吉",
+				"childs": [{
+					"title": "肉钩"
+				}, {
+					"title": "腐烂"
+				}, {
+					"title": "腐肉堆积"
+				}, {
+					"title": "肢解"
+				}]
+			}]
+		}, {
+			"title": "LOL",
+			"childs": []
+		}]
+	}]
+}
